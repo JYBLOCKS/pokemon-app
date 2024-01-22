c59b4f26410e8739cc870dcd7ad40909723b5516
Jose Arredondo <jose.arredondo@buildto.io>
2024-01-22 15:29:49 -0600

    update

--- app/components/cards/pokemon_card.tsx
+++ app/components/cards/pokemon_card.tsx
@@ -1,18 +1,25 @@
 import { Pokemon } from "@/app/lib/models/definitions";
 import styles from "./pokemon_card.module.css";
-import { fetchGetPokemonSprite } from "@/app/services/fetchPokemons";
+import {
+  fetchGetPokemonSprite,
+} from "@/app/services/fetchPokemons";
 import Link from "next/link";
+import Vibrant from "node-vibrant";
 export const Pokemon_Card = async (pokemon: Pokemon) => {
   const imgName = await fetchGetPokemonSprite(pokemon.name);
+  const palette = await Vibrant.from(imgName).getPalette();
+  const colorHex = palette.Vibrant?.hex ?? 'inherit';
   return (
     <div className={styles.pokemon_card}>
       <img
-        className="w-24 h-24 m-auto"
+        className="w-24 h-24 m-auto mt-4"
         src={imgName}
         alt={pokemon.name}
         loading="lazy"
       />
-      <div className="text-center"><Link href={`pokemon?name=${pokemon.name}`}>{pokemon.name}</Link></div>
+      <div className="text-center mt-5">
+        <Link style={{color : colorHex}} href={`pokemon?name=${pokemon.name}`} >{pokemon.name}</Link>
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { fetchGetPokemons } from "./services/fetchPokemons";
 import { Pokemon_Card } from "./components/cards/pokemon_card";
 
@@ -6,11 +5,22 @@ export default async function Home() {
   const getPokemons = await fetchGetPokemons();
   return (
     <>
-      <div className="flex flex-wrap gap-4 ml-5 w-full">
+      <nav>
+        <div className="w-1/2 pt-4 pb-4 m-auto">
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="search"
+            placeholder="Search..."
+            defaultValue=''
+          />
+        </div>
+      </nav>
+      <div className="flex flex-wrap gap-4 w-full justify-center">
         {getPokemons.map((item) => (
-          <Pokemon_Card name={item.name} url={item.url} />
+          <Pokemon_Card name={item.name} url={item.url} key={item.name} />
         ))}
       </div>
+      <footer className="pb-4"></footer>
     </>
   );
 }
